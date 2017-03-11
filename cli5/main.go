3b8a6fd3ae0e5bbc9811a86331d9998b73fcc376
vincentserpoul <vincent@serpoul.com>
2017-03-11 16:31:04 +0000

    antenna on, read number of tags

--- cli5/main.go
+++ cli5/main.go
@@ -25,35 +25,94 @@ func main() {
 		}()
 		fmt.Println("opened", d.Path)
 
-		/* Set Feature */
-		bufS := make([]byte, 64)
-		bufS[0] = 0x10
-		bufS[1] = 0x03
-		bufS[2] = 0x00
-		fd := dev.device.f.Fd()
-		err = Ioctl(fd, IoctlHIDIOCSFEATURE(64), uintptr(unsafe.Pointer(&bufS[0])))
-		if err != nil {
-			log.Fatal(err)
-		} else {
-			log.Print("all good, report sent")
-		}
-
-		time.Sleep(100 * time.Millisecond)
-		/* Get Feature */
-		bufG := make([]byte, 64)
-		bufG[0] = 0x10 /* Report Number */
-		err = Ioctl(fd, IoctlHIDIOCGFEATURE(64), uintptr(unsafe.Pointer(&bufG[0])))
+		resp, err := dev.Command(0x10, []byte{0x00})
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		for _, charact := range bufG {
-			if charact != 0 {
-				fmt.Printf("%X ", charact)
+		for i, charact := range resp {
+			if i > 1 && charact != 0 {
+				fmt.Printf("%c", charact)
 			}
+		}
+		fmt.Print("\n")
+
+		resp, err = dev.Command(0x10, []byte{0x01})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for i, charact := range resp {
+			if i > 1 && charact != 0 {
+				fmt.Printf("%c", charact)
+			}
+		}
+		fmt.Print("\n")
+
+		resp, err = dev.Command(0x18, []byte{0xff})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, charact := range resp {
+			fmt.Printf("0x%X ", charact)
+		}
+		fmt.Print("\n")
+
+		for {
+			respInv, errInv := dev.Command(0x43, []byte{0x01})
+			if errInv != nil {
+				log.Fatal(errInv)
+			}
+
+			for _, charact := range respInv {
+				fmt.Printf("0x%X ", charact)
+			}
+			fmt.Print("\n")
+
+			respInv2, errInv2 := dev.Command(0x43, []byte{0x02})
+			if errInv2 != nil {
+				log.Fatal(errInv2)
+			}
+
+			for _, charact := range respInv2 {
+				fmt.Printf("0x%X ", charact)
+			}
+			fmt.Print("\n")
+
+			time.Sleep(5 * time.Second)
 		}
 
 	}
+}
+
+func (dev *Device) Command(reportID byte, payload []byte) ([]byte, error) {
+
+	/* Set Feature */
+	bufS := make([]byte, 64)
+	bufS[0] = reportID
+	bufS[1] = byte(len(payload) + 2)
+	for i, payloadItm := range payload {
+		bufS[i+2] = payloadItm
+	}
+
+	fd := dev.device.f.Fd()
+	err := Ioctl(fd, IoctlHIDIOCSFEATURE(64), uintptr(unsafe.Pointer(&bufS[0])))
+	if err != nil {
+		return nil, err
+	}
+
+	// wait a little
+	time.Sleep(10 * time.Millisecond)
+
+	bufG := make([]byte, 64)
+	bufG[0] = reportID
+	err = Ioctl(fd, IoctlHIDIOCGFEATURE(64), uintptr(unsafe.Pointer(&bufG[0])))
+	if err != nil {
+		return nil, err
+	}
+
+	return bufG, nil
 }
 
 const (
@@ -117,26 +176,26 @@ type Device struct {
 	buf    []byte
 }
 
-// Command sends a command and associated data to the device and returns the
-// response.
-func (d *Device) Command(cmd byte, data []byte) ([]byte, error) {
-	d.mtx.Lock()
-	defer d.mtx.Unlock()
+// // Command sends a command and associated data to the device and returns the
+// // response.
+// func (d *Device) Command(cmd byte, data []byte) ([]byte, error) {
+// 	d.mtx.Lock()
+// 	defer d.mtx.Unlock()
 
-	if len(data)+2 > maxMessageLen {
-		return nil, fmt.Errorf("device %s Command: command too long", d.info.Product)
-	}
+// 	if len(data)+2 > maxMessageLen {
+// 		return nil, fmt.Errorf("device %s Command: command too long", d.info.Product)
+// 	}
 
-	d.buf = []byte{cmd}
-	d.buf = append(d.buf, byte(len(data)+2))
-	d.buf = append(d.buf, data...)
+// 	d.buf = []byte{cmd}
+// 	d.buf = append(d.buf, byte(len(data)+2))
+// 	d.buf = append(d.buf, data...)
 
-	if err := d.device.Write(d.buf); err != nil {
-		return nil, err
-	}
+// 	if err := d.device.Write(d.buf); err != nil {
+// 		return nil, err
+// 	}
 
-	return d.readResponse()
-}
+// 	return d.readResponse()
+// }
 
 // Close closes the device and frees associated resources.
 func (d *Device) Close() {
